245a1a6d8ed9ddc38a0b90fbb47e360612062d15
Alex-V0l <lastvolnov@gmail.com>
2025-05-09 13:08:26 +0300

    Properties and tests added

---
+++ src/test/java/Tests/BaseTest.java
@@ -0,0 +1,29 @@
+package Tests;
+
+import WebDriverFactory.TestPropertiesConfig;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+
+import java.time.Duration;
+
+import static WebDriverFactory.WebDriverFactory.createWebDriver;
+
+public class BaseTest {
+
+    WebDriver driver;
+    TestPropertiesConfig configProperties = ConfigFactory.create(TestPropertiesConfig.class, System.getProperties());
+
+    @BeforeEach
+    void setUp() {
+        driver = createWebDriver(configProperties.browser());
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+    }
+}
